b572bcc2739a69a1c18da04549476a30b5fa1385
Mridul Sharma <sharmamridul1612@gmail.com>
2024-06-17 22:19:59 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# DS-Visualiser
+A simple React application for visualising common data structures and manipulating them.
+
+**Deployment:** [gauthamk02.github.io/DS-Visualiser/](https://gauthamk02.github.io/DS-Visualiser/)
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Tabs from './components/Tabs';
+
+function App() {
+
+  return (
+    <div className='bg-body'>
+      <Tabs />
+    </div>
+  );
+}
+
+export default App;
